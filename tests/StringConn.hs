9b659fb157a0a48798821ad07d97e390f786cc95
Sanchayan Maity <sanchayan@sanchayanmaity.net>
2024-03-27 23:09:11 +0530

    Fix tests

--- tests/StringConn.hs
+++ tests/StringConn.hs
@@ -13,7 +13,9 @@ module StringConn where
 
 import           Control.Applicative
 import           Prelude hiding (exp)
+import           Control.Monad (when)
 import           Control.Monad.Except
+import           Control.Monad.Error.Class (MonadError)
 import           Control.Monad.Identity
 import           Control.Monad.Reader
 import           Control.Monad.State
